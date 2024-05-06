dd61fc2eaffc91df641abde50f0fc21988ea518f
Isaiahmill14 <isaiahxmiller@gmail.com>
2024-05-06 13:12:47 -0400

    added some intial styling to the forums page

--- src/components/ForumDetail/ForumDetail.jsx
+++ src/components/ForumDetail/ForumDetail.jsx
@@ -1,11 +1,16 @@
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
+import "./ForumDetail.css"; // Import the CSS file for styling
 
 export default function ForumDetail({ forums }) {
-    return (
-        <div className="forum">
-            <div className="content">{forums.content}</div>
-            <div className="xAccount">{forums.xAccount}</div>
-            <Link to={`/forums/${forums._id}`}>Edit</Link> 
+  return (
+    <div className="forum-container">
+        <div className="forum-card">
+        <div className="content">{forums.content}</div>
+        <div className="xAccount">{forums.xAccount}</div>
+        <Link to={`/forums/${forums._id}`} className="edit-link">
+            Edit
+        </Link>
         </div>
-    )
+    </div>
+  );
 }
